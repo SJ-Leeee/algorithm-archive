d0cbd4136305891385c21188a4c2c2a00ce45539
Lee SeungJun <132549231+SJ-Leeee@users.noreply.github.com>
2026-01-15 14:08:11 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
@@ -1,10 +1,20 @@
-day, night, goal = map(int, input().split())
+import sys
 
-if goal <= day:
-    print(1)  
-else:
-    location = goal - day  
-    one_day = day - night  
-    
-    result = (location + one_day - 1) // one_day
-    print(1 + result)  
+A, B, V = map(int, sys.stdin.readline().split())
+
+
+def solutions(day, night, goal):
+    if goal <= day:
+        return 1
+
+    work = goal - day
+    oneday = day - night
+
+    if work % oneday:
+        return (work // oneday) + 2
+    else:
+        return (work // oneday) + 1
+
+
+answer = solutions(A, B, V)
+print(answer)
