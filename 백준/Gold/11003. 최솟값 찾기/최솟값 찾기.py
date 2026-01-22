cd2d320008fab0fad13c750dba3c73073b4d377b
Lee SeungJun <132549231+SJ-Leeee@users.noreply.github.com>
2026-01-22 23:29:02 +0900

    [Gold I] Title: 최솟값 찾기, Time: 2044 ms, Memory: 710764 KB -BaekjoonHub

--- 백준/Gold/11003. 최솟값 찾기/최솟값 찾기.py
+++ 백준/Gold/11003. 최솟값 찾기/최솟값 찾기.py
@@ -1,3 +1,8 @@
+"""
+큐의길이는 M 만큼
+min 을 리스트에 넣으면 O(n) * O(m)
+"""
+
 from collections import deque
 import sys
 
@@ -9,14 +14,18 @@ ans = []
 dq = deque()
 
 for i in range(N):
+    # 인덱스 범위를 초과하는 것을 모두 삭제한다.
     while dq and dq[0][1] <= i - L:
         dq.popleft()
 
+    # 나보다 큰값들을 전부삭제한다 (어차피 쓸모 없음)
     while dq and dq[-1][0] >= arr[i]:
         dq.pop()
 
+    # 작은값 + 나까지 dq 에 배치
     dq.append((arr[i], i))
 
+    # 첫번째는 항상작은값
     ans.append(dq[0][0])
 
 
