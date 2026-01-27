d01172548b6e4f373cf9f96e7c0a479ff4b88669
Lee SeungJun <132549231+SJ-Leeee@users.noreply.github.com>
2026-01-27 17:32:20 +0900

    [Gold II] Title: 가운데를 말해요, Time: 276 ms, Memory: 120776 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.py
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.py
@@ -1,25 +1,23 @@
 import heapq
 import sys
 
-
-leftheap = []
-rightheap = []
-
+left_heap = []
+right_heap = []
 
 N = int(sys.stdin.readline())
-
-for _ in range(N):
+for i in range(1, N + 1):
     num = int(sys.stdin.readline())
-    if len(leftheap) == 0 or -leftheap[0] > num:
-        heapq.heappush(leftheap, -num)
-    else:
-        heapq.heappush(rightheap, num)
+    if i % 2 != 0:  # 홀수일경우
+        if right_heap and right_heap[0] < num:  # 이상한 상황
+            heapq.heappush(left_heap, -heapq.heappop(right_heap))
+            heapq.heappush(right_heap, num)
+        else:
+            heapq.heappush(left_heap, -num)
+    else:  # 짝수일경우
+        if left_heap and -left_heap[0] > num:  # 이상한 상황
+            heapq.heappush(right_heap, -heapq.heappop(left_heap))
+            heapq.heappush(left_heap, -num)
+        else:
+            heapq.heappush(right_heap, num)
 
-    if len(leftheap) > len(rightheap) + 1:
-        move_node = -heapq.heappop(leftheap)
-        heapq.heappush(rightheap, move_node)
-    elif len(rightheap) > len(leftheap):
-        move_node = heapq.heappop(rightheap)
-        heapq.heappush(leftheap, -move_node)
-
-    print(-leftheap[0])
+    print(-left_heap[0])
