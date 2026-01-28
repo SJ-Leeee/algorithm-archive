ad414fdcba96cf4c2662628b7fa40932dedab5a2
Lee SeungJun <132549231+SJ-Leeee@users.noreply.github.com>
2026-01-29 02:02:27 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 2.12 ms, Memory: 9.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -0,0 +1,42 @@
+from itertools import combinations
+
+def solution(orders, course):
+    answer = []
+    dictionary = {}
+    for order in orders:
+        o_list = list(order)  # 리스트로바꾸고
+        max_cnt = len(o_list)
+        o_list.sort()  # 알파벳순으로 변경
+
+        for cnt in course:  # cnt개의 메뉴구성
+            if max_cnt < cnt:
+                break
+
+            for comb in combinations(o_list, cnt):
+                menu = "".join(comb)
+                if menu in dictionary:
+                    dictionary[menu] += 1
+                else:
+                    dictionary[menu] = 1
+    # 길이별로 최댓값 뽑기
+    for cnt in course:
+        # 해당 길이인 것만 필터
+        candidates = [
+            (menu, count)
+            for menu, count in dictionary.items()
+            if len(menu) == cnt and count >= 2
+        ]
+
+        if not candidates:
+            continue
+
+        max_count = max(c for _, c in candidates)
+
+        for menu, count in candidates:
+            if count == max_count:
+                answer.append(menu)
+    answer.sort()
+
+    return answer
+
+
