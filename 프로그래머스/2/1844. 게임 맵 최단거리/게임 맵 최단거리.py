7a0bb435806ee597341d2f167f33bf6ff6b0a6eb
Lee SeungJun <132549231+SJ-Leeee@users.noreply.github.com>
2026-01-30 14:29:52 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.77 ms, Memory: 9.44 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+
+def solution(maps):
+
+    direct = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # 상하좌우
+    dq = deque([(0, 0)])
+
+    row = len(maps)
+    col = len(maps[0])
+
+    INF = float("inf")
+    map_cnt = [[INF] * col for _ in range(row)]
+    map_cnt[0][0] = 1
+    while dq:
+        r, c = dq.popleft()
+        for dr, dc in direct:
+            nr = r + dr
+            nc = c + dc
+            if 0 > nr or row <= nr or 0 > nc or col <= nc:
+                continue
+            if maps[nr][nc] == 0:
+                continue
+            if map_cnt[nr][nc] != INF:  # 이미 방문했으면 스킵
+                continue
+
+            map_cnt[nr][nc] = map_cnt[r][c] + 1
+            dq.append((nr, nc))
+            # 만약 못가면 pass
+            # 나보다 같거나 낮으면 pass
+            # 벽이면 pass
+            # 그게아니라면 갱신하고 큐에추가
+    return -1 if map_cnt[-1][-1] == INF else map_cnt[-1][-1]
