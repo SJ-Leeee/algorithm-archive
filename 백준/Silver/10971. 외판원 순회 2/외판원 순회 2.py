3051d8197a2989def9d97420d275f7df555cf843
Lee SeungJun <132549231+SJ-Leeee@users.noreply.github.com>
2026-01-31 23:53:09 +0900

    [Silver II] Title: 외판원 순회 2, Time: 124 ms, Memory: 110736 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -2,19 +2,32 @@ from itertools import permutations
 import sys
 
 
+# N = int(sys.stdin.readline())
+
+# t_map = []
+# for i in range(N):
+#     line = list(map(int, sys.stdin.readline().split()))
+#     t_map.append(line)
+
+
 def lotate(t_map, city_order):
     t_sum = 0
     # tmap[0][2] > tmap[2][1] > tmap[1][3]...
-    for i in range(len(t_map)):
-        if i == len(t_map) - 1:
-            cost = t_map[city_order[i]][city_order[0]]
-        else:
-            cost = t_map[city_order[i]][city_order[i + 1]]
+    for i in range(len(city_order) - 1):
+        cost = t_map[city_order[i]][city_order[i + 1]]
 
         if cost == 0:
             return False
 
         t_sum += cost
+
+    first = t_map[0][city_order[0]]
+    last = t_map[city_order[-1]][0]
+
+    if first == 0 or last == 0:
+        return False
+
+    t_sum = t_sum + first + last
 
     return t_sum
 
@@ -26,10 +39,11 @@ for i in range(N):
     line = list(map(int, sys.stdin.readline().split()))
     t_map.append(line)
 
-cities = [i for i in range(N)]
+cities = [i for i in range(1, N)]
+# 0번 하자
 
-min_cost = float('inf')
-for perm in permutations(cities, N):
+min_cost = float("inf")
+for perm in permutations(cities, N - 1):
     ret = lotate(t_map, perm)
     if not ret:
         continue
