d68a2980dc8c698f4e0db55c9d069ecfe62a25f1
Lee SeungJun <132549231+SJ-Leeee@users.noreply.github.com>
2026-02-02 02:26:10 +0900

    [Gold V] Title: 하노이 탑, Time: 448 ms, Memory: 149500 KB -BaekjoonHub

--- 백준/Gold/1914. 하노이 탑/하노이 탑.py
+++ 백준/Gold/1914. 하노이 탑/하노이 탑.py
@@ -1,29 +1,17 @@
-count = 0
+import sys
 
 
-def swap(num, location_num, destination_num, N):
-    if num == 1:
-        if N <= 20:
-            print(f"{location_num} {destination_num}")
+N = int(sys.stdin.readline())
+
+
+def hanoi_2(n, start, end, mid):
+    if n == 1:
+        print(start, end)
         return
+    hanoi_2(n - 1, start, mid, end)
+    print(start, end)
+    hanoi_2(n - 1, mid, end, start)
 
-    new_destination = 6 - location_num - destination_num
-
-    swap(num - 1, location_num, new_destination, N)
-
-    if N <= 20:
-        print(f"{location_num} {destination_num}")
-
-    swap(num - 1, new_destination, destination_num, N)
-
-    return
-
-
-num = int(input())
-
-if num > 20:
-    print(2**num - 1)
-else:
-    print(2**num - 1)
-    swap(num, 1, 3, num)
-# 수학 공식으로 바로 계산
+print(2**N -1)
+if N <= 20:
+    hanoi_2(N, 1, 3, 2)
