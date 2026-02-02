a13f5e4a10a2f1515d8bef6dfd0c5afb8cad2de1
Lee SeungJun <132549231+SJ-Leeee@users.noreply.github.com>
2026-02-02 17:25:34 +0900

    [Silver V] Title: 배열 합치기, Time: 760 ms, Memory: 343680 KB -BaekjoonHub

---
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.py
@@ -0,0 +1,28 @@
+import sys
+
+
+N, M = map(int, sys.stdin.readline().split())
+list1 = list(map(int, sys.stdin.readline().split()))
+list2 = list(map(int, sys.stdin.readline().split()))
+
+answer = []
+i = 0
+j = 0
+
+
+while i < N and j < M:
+    if list1[i] < list2[j]:
+        answer.append(list1[i])
+        i += 1
+    else:
+        answer.append(list2[j])
+        j += 1
+
+while i < N:
+    answer.append(list1[i])
+    i += 1
+
+while j < M:
+    answer.append(list2[j])
+    j += 1
+print(*answer)
