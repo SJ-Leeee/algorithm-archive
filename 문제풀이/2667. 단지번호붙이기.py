a57d5ffd5cabc6a769adc1be119be4a956484e66
leeseungjun <cjstkrak@gmail.com>
2026-02-04 19:47:31 +0900

    dfs: 단지번호붙이기

---
+++ 문제풀이/2667. 단지번호붙이기.py
@@ -0,0 +1,42 @@
+"""
+
+7
+0110100
+0110101
+1110101
+0000111
+0100000
+0111110
+0111000
+"""
+
+import sys
+
+
+def dfs(row, col, visited):
+    visited[row][col] = True
+    cnt = 1
+    for dr, dc in direct:
+        nr, nc = row + dr, col + dc
+        if 0 <= nr < N and 0 <= nc < N:
+            if not visited[nr][nc] and villages[nr][nc] == 1:
+                cnt += dfs(nr, nc, visited)
+
+    return cnt
+
+
+N = int(sys.stdin.readline())
+villages = [[int(x) for x in sys.stdin.readline().strip()] for _ in range(N)]
+visited = [[False] * N for _ in range(N)]
+direct = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+ans = []
+for i in range(N):
+    for j in range(N):
+        if not visited[i][j] and villages[i][j] == 1:
+            ans.append(dfs(i, j, visited))
+
+ans.sort()
+print(len(ans))
+for i in ans:
+    print(i)
