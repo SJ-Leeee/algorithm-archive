ebd229c5fa899413a33011139e3f53f4e76c81c6
leeseungjun <cjstkrak@gmail.com>
2026-02-04 20:00:12 +0900

    dfs: 유기농배추

---
+++ 문제풀이/1012. 유기농배추.py
@@ -0,0 +1,60 @@
+"""
+2
+10 8 17 가로 세로 배추위치
+0 0
+1 0
+1 1
+4 2
+4 3
+4 5
+2 4
+3 4
+7 4
+8 4
+9 4
+7 5
+8 5
+9 5
+7 6
+8 6
+9 6
+10 10 1
+5 5
+"""
+
+import sys
+
+sys.setrecursionlimit(10**5)
+
+
+def dfs(r, c, visited):
+    for dr, dc in direct:
+        nr, nc = r + dr, c + dc
+        if 0 <= nr < R and 0 <= nc < C:
+            if not visited[nr][nc] and farm[nr][nc] == 1:
+                visited[nr][nc] = True
+                dfs(nr, nc, visited)
+
+
+T = int(sys.stdin.readline())
+direct = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+for _ in range(T):
+    C, R, M = map(int, sys.stdin.readline().split())
+    farm = [[0] * C for _ in range(R)]
+    visited = [[False] * C for _ in range(R)]
+
+    ans = 0
+
+    for _ in range(M):
+        c, r = map(int, sys.stdin.readline().split())
+        farm[r][c] = 1
+
+    for i in range(R):
+        for j in range(C):
+            if not visited[i][j] and farm[i][j] == 1:
+                visited[i][j] = True
+                dfs(i, j, visited)
+                ans += 1
+
+    print(ans)
