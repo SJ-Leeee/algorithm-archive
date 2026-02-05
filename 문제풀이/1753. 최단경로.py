da78dbd752ca65843fd8552a6ce86253df9df64e
leeseungjun <cjstkrak@gmail.com>
2026-02-05 23:16:33 +0900

    dijkstra: 최단경로

---
+++ 문제풀이/1753. 최단경로.py
@@ -0,0 +1,54 @@
+"""
+5 6
+1
+5 1 1
+1 2 2
+1 3 3
+2 3 4
+2 4 5
+3 4 6
+"""
+
+# dijkstra
+
+import heapq
+import sys
+
+
+def dijkstra(start, graph):
+    distance = [float("inf")] * (len(graph) + 1)
+    distance[start] = 0
+
+    pq = [(0, start)]
+
+    while pq:
+        cost, v = heapq.heappop(pq)
+        if cost > distance[v]:
+            continue
+
+        for nv, nc in graph[v]:
+            new_cost = nc + cost
+            if new_cost < distance[nv]:
+                distance[nv] = new_cost
+                heapq.heappush(pq, (new_cost, nv))
+
+    return distance
+
+
+V, E = map(int, sys.stdin.readline().split())
+start = int(sys.stdin.readline())
+graph = {}
+
+for i in range(1, V + 1):
+    graph[i] = []
+
+for _ in range(E):
+    v1, v2, cost = map(int, sys.stdin.readline().split())
+    graph[v1].append((v2, cost))
+
+dist = dijkstra(start, graph)
+for i in dist[1:]:
+    if i == float("inf"):
+        print("INF")
+    else:
+        print(i)
