26f976407b0277070ccbf0df6853cce67fc2b0d0
leeseungjun <cjstkrak@gmail.com>
2026-02-08 12:55:31 +0900

    벨만포드: 타임머신

---
+++ 문제풀이/11657. 타임머신.py
@@ -0,0 +1,43 @@
+"""
+3 4
+1 2 4
+1 3 3
+2 3 -1
+3 1 -2
+"""
+
+import sys
+
+
+def bellman_ford(n, start, edges):
+    INF = float("inf")
+    dist = [INF] * (n + 1)
+    dist[start] = 0
+
+    for _ in range(n - 1):
+        for a, b, cost in edges:
+            if dist[a] != INF and dist[a] + cost < dist[b]:
+                dist[b] = dist[a] + cost
+
+    for a, b, cost in edges:
+        if dist[a] != INF and dist[a] + cost < dist[b]:
+            return -1
+
+    return dist
+
+
+V, E = map(int, sys.stdin.readline().split())
+
+edges = []
+for _ in range(E):
+    edge = list(map(int, sys.stdin.readline().split()))
+    edges.append(edge)
+
+ans = bellman_ford(V, 1, edges)
+
+if ans == -1:
+    print(-1)
+else:
+    ans = [-1 if x == float("inf") else x for x in ans[2:]]
+    for i in ans:
+        print(i)
